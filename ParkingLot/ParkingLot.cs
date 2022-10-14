5c4c0232cda41b4da17eeeffa4bb221a30dd6989
KUNWEI <kwei@thoughtworks.com>
2022-10-14 13:55:35 +0800

    feat: use a ticket pickup a car

--- ParkingLot/ParkingLot.cs
+++ ParkingLot/ParkingLot.cs
@@ -5,12 +5,17 @@ namespace ParkingLot
     using System;
     public class ParkingLot
     {
-        private Dictionary<string, Car> parkingLots;
+        private Dictionary<string, Car> parkingLots = new Dictionary<string, Car>();
         public string Parking(Car car)
         {
             string ticket = TicketGenerator.CreateTicket();
             parkingLots.Add(ticket, car);
             return ticket;
+        }
+
+        public Car Pickup(string ticket)
+        {
+            return parkingLots[ticket];
         }
     }
 }
--- ParkingLotTest/ParkingLotTest.cs
+++ ParkingLotTest/ParkingLotTest.cs
@@ -14,5 +14,16 @@ namespace ParkingLotTest
 
             Assert.Matches("^T\\d{18}$", ticketNumber);
         }
+
+        [Fact]
+        public void Should_pickup_a_parked_car_when_pickup_at_parking_lot_given_a_ticket()
+        {
+            var parkingLot = new ParkingLot();
+            var ticketNumber = parkingLot.Parking(new Car("AE8888"));
+
+            Car car = parkingLot.Pickup(ticketNumber);
+
+            Assert.Equal("AE8888", car.PlantNumber);
+        }
     }
 }
